602c909e563a51326254c0f5dafc1e5bc987fd8d
Partially Sorted <partially.sorted@gmail.com>
2022-01-08 10:57:22 -0600

    Reorganized directory structure.

---
+++ tests/yyprime-exchange.ts
@@ -0,0 +1,17 @@
+import * as anchor from '@project-serum/anchor';
+import { Program } from '@project-serum/anchor';
+import { YyprimeExchange } from '../target/types/yyprime_exchange';
+
+describe('yyprime-exchange', () => {
+
+  // Configure the client to use the local cluster.
+  anchor.setProvider(anchor.Provider.env());
+
+  const program = anchor.workspace.YyprimeExchange as Program<YyprimeExchange>;
+
+  it('Is initialized!', async () => {
+    // Add your test here.
+    const tx = await program.rpc.initialize({});
+    console.log("Your transaction signature", tx);
+  });
+});
