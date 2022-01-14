71a919bb3d5770601372a77ea059e4b33fda978e
Partially Sorted <partially.sorted@gmail.com>
2022-01-13 23:31:10 -0600

    Added PythClient.query back just in case we need it one day.

--- packages/yyprime-exchange-ts/src/pyth/pyth-client.ts
+++ packages/yyprime-exchange-ts/src/pyth/pyth-client.ts
@@ -86,4 +86,44 @@ export class PythClient {
     );
   }
 
+  /*
+  public async query() {
+    let products: {}[] = [];
+
+    const programAccounts = await this.connection.getProgramAccounts(this.pythProgram, this.commitment);
+    programAccounts.forEach(account => {
+      const base = parseBaseData(account.account.data);
+      if (base != null) {
+        if (AccountType[base.type] == 'Product') {
+          const product = parseProductData(account.account.data)
+          products.push({
+            productAddress: account.pubkey.toBase58(),
+            priceAddress: new PublicKey(product.priceAccountKey).toBase58(),
+            ...product.product,
+          });
+        }
+      }
+    });
+
+    / *
+    const mappingAccount = await this.connection.getAccountInfo(new PublicKey(ORACLE_MAPPING_PUBLIC_KEY))
+    const mappingData = parseMappingData(mappingAccount!.data)
+    if (mappingData.nextMappingAccount != null) {
+      throw new Error("Implement.");
+    }
+    for (let i = 0; i < mappingData.productAccountKeys.length; i++) {
+      const productAccount = await this.connection.getAccountInfo(new PublicKey(mappingData.productAccountKeys[i]))
+      const product = parseProductData(productAccount!.data)
+      products.push({
+        productAddress: new PublicKey(mappingData.productAccountKeys[i]).toBase58(),
+        priceAddress: new PublicKey(product.priceAccountKey).toBase58(),
+        ...product.product,
+      });
+    }
+    * /
+
+    return products;
+  }
+  */
+
 }
