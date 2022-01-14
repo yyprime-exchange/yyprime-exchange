8854c590c4b474d5eb91a0bacd697b0f5a2f4639
Partially Sorted <partially.sorted@gmail.com>
2022-01-13 23:13:02 -0600

    Cleaned up Pyth code.

--- packages/yyprime-exchange-ts/src/pyth/pyth-client.ts
+++ packages/yyprime-exchange-ts/src/pyth/pyth-client.ts
@@ -10,22 +10,27 @@ import {
   parseBaseData,
   parsePriceData,
   parseProductData,
-  PriceData,
-  ProductData,
 } from '@pythnetwork/client'
+
+export interface Price {
+  price: number | undefined;
+  confidence: number | undefined;
+  priceKey: PublicKey
+}
 
 export interface Product {
   symbol: string
   baseSymbol: string
   quoteSymbol: string
-  product: PublicKey
-  price: PublicKey
+  productKey: PublicKey
+  priceKey: PublicKey
 }
 
 export class PythClient {
   cluster: string;
   commitment: Commitment = 'finalized';
   connection: Connection;
+  onPrice: (price: Price, product: Product) => void;
   products: Map<string, Product>;
   pythProgram: PublicKey;
 
@@ -33,83 +38,31 @@ export class PythClient {
     cluster: string,
     program: string,
     url: string,
+    onPrice: (price: Price, product: Product) => void,
   ) {
     this.cluster = cluster;
     this.connection = new Connection(url);
+    this.onPrice = onPrice;
     this.products = new Map<string, Product>();
     this.pythProgram = new PublicKey(program);
-  }
-
-  public onPrice(price: PriceData, product: Product) {
-    //TODO callback.
-    console.log(`[PRICE]`);
-    console.log(`product = ${product.symbol}`);
-    console.log(`price = ${price.price}`);
-    console.log(`confidence = ${price.confidence}`);
-    console.log('');
-  }
-
-  // Query the products listed onchain.
-  public async queryProducts2() {
-    let products: {}[] = [];
-
-    const programAccounts = await this.connection.getProgramAccounts(this.pythProgram, this.commitment);
-    programAccounts.forEach(account => {
-      const base = parseBaseData(account.account.data);
-      if (base != null) {
-        if (AccountType[base.type] == 'Product') {
-          const product = parseProductData(account.account.data)
-          products.push({
-            productAddress: account.pubkey.toBase58(),
-            priceAddress: new PublicKey(product.priceAccountKey).toBase58(),
-            ...product.product,
-          });
-        }
-      }
-    });
-
-    /*
-    const mappingAccount = await this.connection.getAccountInfo(new PublicKey(ORACLE_MAPPING_PUBLIC_KEY))
-    const mappingData = parseMappingData(mappingAccount!.data)
-    if (mappingData.nextMappingAccount != null) {
-      throw new Error("Implement.");
-    }
-    for (let i = 0; i < mappingData.productAccountKeys.length; i++) {
-      const productAccount = await this.connection.getAccountInfo(new PublicKey(mappingData.productAccountKeys[i]))
-      const product = parseProductData(productAccount!.data)
-      products.push({
-        productAddress: new PublicKey(mappingData.productAccountKeys[i]).toBase58(),
-        priceAddress: new PublicKey(product.priceAccountKey).toBase58(),
-        ...product.product,
-      });
-    }
-    */
-
-    return products;
   }
 
   public async subscribe() {
     (await this.connection.getProgramAccounts(this.pythProgram, this.commitment)).forEach(account => {
       const base = parseBaseData(account.account.data);
       if (base != null) {
-        if (AccountType[base.type] == 'Price') {
-          const priceData = parsePriceData(account.account.data)
-          const product = this.products.get(account.pubkey.toBase58());
-          if (product) {
-            this.onPrice(priceData, product);
-          }
-        }
-        else if (AccountType[base.type] == 'Product') {
+        if (AccountType[base.type] == 'Product') {
           const product = account.pubkey;
           const productData = parseProductData(account.account.data)
-          console.log(JSON.stringify(productData));
-          this.products.set(productData.priceAccountKey.toBase58(), {
-            symbol: productData.product.symbol,
-            baseSymbol: productData.product.baseSymbol,
-            quoteSymbol: productData.product.quote_currency,
-            product: product,
-            price: productData.priceAccountKey,
-          });
+          if (productData.product.quote_currency === 'USD' && productData.product.asset_type === 'Crypto') {
+            this.products.set(productData.priceAccountKey.toBase58(), {
+              symbol: productData.product.symbol,
+              baseSymbol: productData.product.base,
+              quoteSymbol: productData.product.quote_currency,
+              productKey: product,
+              priceKey: productData.priceAccountKey,
+            });
+          }
         }
       }
     });
@@ -121,9 +74,10 @@ export class PythClient {
         if (base != null) {
           if (AccountType[base.type] == 'Price') {
             const price = parsePriceData(keyedAccountInfo.accountInfo.data)
-            const product = this.products.get(keyedAccountInfo.accountId.toBase58());
+            const priceKey = keyedAccountInfo.accountId;
+            const product = this.products.get(priceKey.toBase58());
             if (product) {
-              this.onPrice(price, product);
+              this.onPrice({ price: price.price, confidence: price.confidence, priceKey: priceKey }, product);
             }
           }
         }
--- scripts/monitor.ts
+++ scripts/monitor.ts
@@ -1,14 +1,28 @@
-import { PythClient } from '../packages/yyprime-exchange-ts/lib';
+import { Price, Product, PythClient } from '../packages/yyprime-exchange-ts/lib';
 
 import * as simulation from './simulation.json';
 
 console.log(`Monitoring simulation on ${simulation.config.cluster}`);
 
+const pythClient: PythClient = new PythClient(
+  simulation.config.cluster,
+  simulation.config.pyth.program,
+  simulation.config.pyth.url,
+  (price: Price, product: Product) => {
+    console.log(`[PRICE]`);
+    console.log(`product = ${product.symbol}`);
+    console.log(`price = ${price.price}`);
+    console.log(`confidence = ${price.confidence}`);
+    console.log('');
+  },
+);
 
-const pythClient: PythClient = new PythClient(simulation.config.cluster, simulation.config.pyth.program, simulation.config.pyth.url);
 pythClient.subscribe();
 
-setInterval(() => {
+
+
+//setInterval(() => {
   //TODO
 
-}, 1000);
+
+//}, 1000);
