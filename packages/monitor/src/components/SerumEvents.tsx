8f5f6204dbab009237eee385fb28458f71970570
AG <10.2aaron@gmail.com>
2022-01-22 21:55:05 -0600

    a bunch of regex mess to clean a little the serum table output

--- packages/monitor/src/components/SerumEvents.tsx
+++ packages/monitor/src/components/SerumEvents.tsx
@@ -16,7 +16,9 @@ export default function SerumEvents() {
     <FloatingElement style={{ width: '400px', height: '450px' }} >
       <Title>Serum Events</Title>
       {events.slice(0, 7).map((event, index) => (
-        <Row key={index}><pre>{JSON.stringify(event)}</pre></Row>
+        <Row key={index}><pre>{
+          JSON.stringify(event).replace(/[\{,]*(false)|[:]( true,)|(:,)/g,"").replace(/[}{"]|( : )|(: )/g," ").replace(/[,]*(bid)/g,"").replace(":,","").replaceAll(":true,","").replaceAll("  "," ")
+        } </pre></Row>
       ))}
     </FloatingElement>
   );
